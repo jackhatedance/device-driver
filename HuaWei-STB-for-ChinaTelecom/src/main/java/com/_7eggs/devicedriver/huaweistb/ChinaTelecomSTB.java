fd5e26271a3854b51a034e77133d515eb2bb6248
jack <jack@192.168.0.107>
2014-07-06 20:22:42 +0800

    add stb driver

---
+++ HuaWei-STB-for-ChinaTelecom/src/main/java/com/_7eggs/devicedriver/huaweistb/ChinaTelecomSTB.java
@@ -0,0 +1,46 @@
+package com._7eggs.devicedriver.huaweistb;
+
+import com.deviceyun.smarthome.api.device.AbstractDevice;
+import com.deviceyun.smarthome.api.device.transmitter.IrTransmitter;
+import com.deviceyun.smarthome.api.device.tv.TV;
+
+public class ChinaTelecomSTB extends AbstractDevice implements TV {
+	private IrTransmitter controller;
+
+	private static long CODE_CHANNEL[] = { 0x12345, 0x123456 };
+
+	public ChinaTelecomSTB() {
+		configure.put("bits", 24);
+		configure.put("repeat", 1);
+	}
+
+	public void setController(IrTransmitter controller) {
+		this.controller = controller;
+	}
+
+	
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setVolume(int vol) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void switchToChannel(int channel) {
+		controller.transmit(IrTransmitter.TYPE_NEC, CODE_CHANNEL[channel],
+				configure.getInt("bits"), configure.getInt("repeat"));
+
+	}
+}
