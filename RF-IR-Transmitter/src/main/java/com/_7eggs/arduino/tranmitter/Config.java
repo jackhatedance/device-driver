da898d2da8b3b8dfec7fa5175b350e326e0cfb14
jack <jackhatedance@gmail.com>
2014-09-03 23:42:57 +0800

    update with yunos, add config def item order.

--- RF-IR-Transmitter/src/main/java/com/_7eggs/arduino/tranmitter/Config.java
+++ RF-IR-Transmitter/src/main/java/com/_7eggs/arduino/tranmitter/Config.java
@@ -3,12 +3,12 @@ package com._7eggs.arduino.tranmitter;
 import com.driverstack.yunos.driver.config.annotation.Configure;
 import com.driverstack.yunos.driver.config.annotation.Item;
 
-@Configure(resourceFile="config/config",supportedlocales = {
-		"en_US", "zh_CN" })
+@Configure(resourceFile = "config/config", supportedlocales = { "en_US",
+		"zh_CN" })
 public class Config {
-	@Item
+	@Item(order = 1)
 	private String host;
-	@Item
+	@Item(order = 2)
 	private int port;
 
 	public String getHost() {
