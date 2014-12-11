afbd535640ece447ce24f3b4a6277d722221b62d
jack <jackhatedance@gmail.com>
2014-12-11 23:50:30 +0800

    RcSwitch driver 2.0 support stateful switch device

--- RCSwitchSocketDriver/src/main/java/com/_7eggs/devicedriver/rcswitch/RcSwitchFD.java
+++ RCSwitchSocketDriver/src/main/java/com/_7eggs/devicedriver/rcswitch/RcSwitchFD.java
@@ -1,11 +1,14 @@
 package com._7eggs.devicedriver.rcswitch;
 
+import com.driverstack._interface._switch.StatefulElectricitySwitch;
 import com.driverstack.yunos.deviceApi._switch.ElectricitySwitch;
 import com.driverstack.yunos.deviceApi.transmitter.RfTransmitter;
 
-public class RcSwitchFD implements ElectricitySwitch {
+public class RcSwitchFD implements StatefulElectricitySwitch {
 	private Config config;
 	private RfTransmitter controller;
+
+	private boolean stateOn = false;
 
 	public RcSwitchFD(Config config) {
 		this.config = config;
@@ -18,13 +21,21 @@ public class RcSwitchFD implements ElectricitySwitch {
 
 		controller.transmit(RfTransmitter.FREQUENCY_433,
 				config.getPulseLength(), config.getCodeOn(), config.getBits());
+		stateOn = true;
 	}
 
 	@Override
 	public void off() {
 		controller.transmit(RfTransmitter.FREQUENCY_433,
 				config.getPulseLength(), config.getCodeOff(), config.getBits());
+		stateOn = false;
 
+	}
+
+	@Override
+	public boolean isOn() {
+
+		return stateOn;
 	}
 
 }
